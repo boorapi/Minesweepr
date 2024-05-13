5a2b285a3227eeb308f4c4174fe3efb060cac260
boorapi <169002265+boorapi@users.noreply.github.com>
2024-05-13 13:12:38 +1200

    Add files via upload
    1st commit

---
+++ MineswepperGame_Pinn/GithubTest/Test.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    static void main(String args[]){
+        //helo world!!
+    }
+}
