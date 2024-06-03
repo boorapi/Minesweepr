6eded1a50ab1b694be1eca4265062c767f6b6bc0
boorapi <boorapi@whs.school.nz>
2024-06-03 14:33:49 +1200

    03/06/24
    Finished the error checking and finish making the recursive call.

--- Game/MinesweeperV1.java
+++ Game/MinesweeperV1.java
@@ -16,20 +16,25 @@ public class MinesweeperV1
     
     static void plantBomb(){
         Random random = new Random();
+        for (int x=0; x<10; x++){
+            for (int y=0; y<10; y++){
+                board[x][y] = "o";
+            }
+        }
         // not mvp; make sure that no same locatoin  genarated.
-        for (int j=0; j<15; j++){
+        for (int j=0; j<11; j++){
             int x = random.nextInt(10);
             int y = random.nextInt(10);
-            board[x][y] = "B";
+            board[x][y] = "b";
         }
     }
     //check the status on that particular cell.
-    static int checkbomb(int x, int y){
+    static int countbomb(int x, int y){
         //if it out of board rage return 0.
-        if ( x < 0 || x > 10 || y < 0 || y > 10){
+        if ( x < 0 || x >= 10 || y < 0 || y >= 10){
             return 0;
         }
-        else if (board[x][y].equals("B")){
+        else if (board[x][y].equals("b")){
             return 1;
         }
         else{
@@ -38,32 +43,47 @@ public class MinesweeperV1
     }
     
     // set up the table.
-    static void CountBomb(){
-        plantBomb();
+    static void CheckBomb(int x, int y){
         //couting the surounding bomb.
-        for (int x=0; x<10; x++){
-            for (int y=0; y<10; y++){
-                int BombCount = 0;
-                // if that cell is not a bomb then check it.
-                if (board[x][y] != "x"){
-                    //top three
-                    BombCount += checkbomb(x-1, y-1);
-                    BombCount += checkbomb(x, y-1);
-                    BombCount += checkbomb(x+1, y-1); 
-                    //left
-                    BombCount += checkbomb(x-1, y);
-                    //right
-                    BombCount += checkbomb(x+1, y);
-                    //bottom three
-                    BombCount += checkbomb(x-1, y+1);
-                    BombCount += checkbomb(x, y+1);
-                    BombCount += checkbomb(x+1, y+1);
-                    
-                    String BombNum = Integer.toString(BombCount);
-                    board[x][y] = BombNum;
-                }
-            }
-         }
+        int BombCount = 0;
+        
+        if ( x < 0 || x >= 10 || y < 0 || y >= 10 || board[x][y].equals("0")){
+            return;
+        }
+        
+
+        //top three
+        BombCount += countbomb(x-1, y-1);
+        BombCount += countbomb(x, y-1); 
+        BombCount += countbomb(x+1, y-1); 
+        //left
+        BombCount += countbomb(x-1, y);
+        //right
+        BombCount += countbomb(x+1, y);
+        //bottom three
+        BombCount += countbomb(x-1, y+1);
+            
+        BombCount += countbomb(x, y+1);
+        BombCount += countbomb(x+1, y+1);
+            
+        String BombNum = Integer.toString(BombCount);
+        if (BombCount > 0){
+            board[x][y] = BombNum;
+        }
+        else {
+            board[x][y] = "0";    
+            //recursive call to check surounding zero.
+            CheckBomb(x-1, y-1);
+            CheckBomb(x, y-1);
+            CheckBomb(x+1, y-1);
+                
+            CheckBomb(x-1, y);
+            CheckBomb(x+1, y);
+            
+            CheckBomb(x-1, y+1);
+            CheckBomb(x, y+1);
+            CheckBomb(x+1, y+1);
+        }
     }
     //print the display out.
     static void printDisplay(){
@@ -72,64 +92,99 @@ public class MinesweeperV1
         for  (int x=0; x<10; x++){
             System.out.print(coor[x] + "  " );
             for (int y=0; y<10; y++){
-                //display[x][y] = "o";
-                System.out.print(display[x][y] + " ");
+        System.out.print(board[x][y] + " ");
             }
             System.out.println();
         }    
     }
-    public static void main(String args[]){
+    //error checking for int 1 - 10 for x axis.
+    static int numCheck(String msg){
+        Scanner kb  = new Scanner(System.in);
+        System.out.print(msg + " For X axis: ");
+        while(!kb.hasNextInt()){
+            kb.nextLine();
+            System.out.print("Input error! " + msg);
+        }
+        return(kb.nextInt());
+    }
+    //error checking for commands.
+    static String comCheck(String[] commands, String msg){
         Scanner kb = new Scanner(System.in);
-        //set up the table
-        CountBomb();
+        String choice = "";
         
+        while (!Arrays.asList(commands).contains(choice)){
+            System.out.print(msg);
+            choice = kb.nextLine();
+            choice = choice.toLowerCase();
+        }
+        return (choice);
+    }
+    
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
         
+        //set up the table and the display.
+        plantBomb();
+        int bombAmount = 0;
         for  (int x=0; x<10; x++){
             for (int y=0; y<10; y++){
                 display[x][y] = "o";
+                if (board[x][y].equals("b")){
+                    bombAmount ++;
+                }
             }
         }
         printDisplay();
-
+        
         boolean game = true;
         int flag = 0;
         System.out.println("wellcome  to Minesweeper game!!");
         while (game){
-            System.out.print("Type flag to flaged, dig to dig or undo to undo the flag: ");
-            String choice = kb.nextLine();
+            System.out.println('\u000c');
+            printDisplay();
             
-            System.out.print("Type in the coordinate \nfor X axis: ");
-            int x = kb.nextInt();
-            kb.nextLine();
-            System.out.print("for Y axis: ");
-            String y = kb.nextLine();
+            System.out.println("What would you like to do? ");
+            
+            //get and error checking the command input.
+            String[] commands = {"flag", "undo", "dig"};
+            String choice = comCheck(commands, "Pleas type flag to flaged, dig to dig or undo to undo the flag: ");
+            
+            System.out.println("Type in the coordinate.");
+            
+            //get and error ckecking the input for x coordinate.
+            int x = 0;
+            while(x < 1 || x > 10){
+                x = numCheck("Please type a number between 1 - 10:  ");
+            }
+            x -= 1;
+            
+            //get and error checking the input for y axis
+            String[] comY = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+            String y = comCheck( comY, "Pleas type a alphabet from a - j for Y axis: ");
+            
             y = y.toUpperCase();
             //get the upper case y vriable convert it in to char 
             char Yaxis = y.charAt(0);
-            x -= 1;
             
-            if  (choice.equals("flag")){
+             if (choice.equals("flag")){
                 // Yaxis - 64 to get the correct location for y axis (A = 65 in char).
                 display[Yaxis-65][x] = "f";
                 flag ++;
-                if (flag == 14){
+                if (flag == bombAmount){
                     game = false;
                 }
             }else if(choice.equals("undo")){
                 display[Yaxis-65][x] = "o";
                 flag --;
             }else if(choice.equals("dig")){
-                if (board[Yaxis-65][x].equals("B")){
+                if (board[Yaxis-65][x].equals("b")){
+                    System.out.println("You dig a bomb. You lose!!!");
                     game = false;
                 }
                 else{
-                    display[Yaxis-65][x] = board[Yaxis-65][x];
+                    CheckBomb(x, Yaxis-65);
                 }
             }
-            System.out.println('\u000c');
-            printDisplay();
         }
-        System.out.println("You dig a bomb. You lose!!!");
-        
     }
 }
